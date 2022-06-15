3e52e042e92c291a1defedaec5955031f96d0338
tvpeter <withtvpeter@gmail.com>
2022-06-15 05:49:16 +0100

    add search functionality

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,9 @@ pub fn run (config: Config)-> Result<(), Box<dyn Error>> {
 
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("with text: \n {}", contents);
+    for line in search(&config.query, &contents){
+        println!("{}", &line);
+    }
 
     Ok(())
 }
@@ -28,4 +30,32 @@ pub fn new(args: &[String]) -> Result<Config, &str> {
    Ok(Config { query, filename })
 }
     
+}
+
+pub fn search<'a>(query: &str, contents: &'a str)-> Vec<&'a str> {
+    let mut result = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query){
+            result.push(line);
+        }
+    }
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result (){
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, productive, fast.
+pick three.";
+
+            assert_eq!(vec!["safe, productive, fast."], search(query, contents));
+    }
 }
